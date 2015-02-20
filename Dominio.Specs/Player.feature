e0e29422140d8930184ff939b9389b3fe9256892
Wendy Paz <deirdrak@gmail.com>
2015-02-20 04:04:35 -0600

    player spec

---
+++ Dominio.Specs/Player.feature
@@ -0,0 +1,20 @@
+﻿Feature: Player
+	
+
+@mytag
+Scenario: Player one gets a tile from stock
+	#Given a board
+	Given a stock
+	| Tile | head | tail |
+	| 1    | 3    | 4    |
+	| 2    | 1    | 0    |
+	| 3    | 6    | 6    |
+	| 4    | 2    | 3    |
+	And Player 1 doesn't have a matching piece
+	| Tile | head | tail |
+	| 1    | 1    | 4    |
+	| 2    | 5    | 6    |
+	| 3    | 0    | 5    |
+	| 4    | 0    | 3    |
+	Then add 1 tile to Player 1 list of tiles
+	And remove 1 tile from stock
---
+++ Dominio.Specs/PlayerSteps.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+using TechTalk.SpecFlow;
+using TechTalk.SpecFlow.Assist;
+
+namespace Dominio.Specs
+{
+    [Binding]
+    public class PlayerSteps
+        {
+            private List<Tile> _stock;
+            private List<Tile> _playerTilesList;
+            
+        //[Given(@"a board")]
+        //public void GivenABoard()
+        //{
+        //    ScenarioContext.Current.Pending();
+        //}
+        
+        [Given(@"a stock")]
+        public void GivenAStock(Table table)
+        {
+            _stock = table.CreateSet<Tile>().ToList();
+        }
+        
+        [Given(@"Player (.*) doesn't have a matching piece")]
+        public void GivenPlayerDoesnTHaveAMatchingPiece(int p0, Table table)
+        {
+            _playerTilesList = table.CreateSet<Tile>().ToList();
+        }
+
+        [Then(@"add (.*) tile to Player (.*) list of tiles")]
+        public void ThenAddTileToPlayerListOfTiles(int p0, int p1)
+        {
+            for (int i = 0; i < p0; i++)
+            {
+                _playerTilesList.Add(_stock.ElementAt(0));
+            }
+            
+        }
+
+        [Then(@"remove (.*) tile from stock")]
+        public void ThenRemoveTileFromStock(int p0)
+        {
+            for (int i = 0; i < p0; i++)
+            {
+                _stock.RemoveAt(0);
+            }
+        }
+
+
+    }
+
+    internal class Tile
+        {
+            public int Head { get; set; }
+            public int Tail { get; set; }
+        }
+}
